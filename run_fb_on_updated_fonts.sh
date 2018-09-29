5257b546696a103cddbebb66d9c3573e16acd2b8
Marc Foley <m.foley.88@gmail.com>
2018-09-29 16:47:17 +0100

    update travis

--- run_fb_on_updated_fonts.sh
+++ run_fb_on_updated_fonts.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-pr_files=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
-
-fontbakery check-googlefonts $pr_files -l FAIL
+echo "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files"
+file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
+echo $file_names
+fontbakery check-googlefonts $file_names -l FAIL
