32aa674ff58d8860cfad2d8fdec97d7743d10b1e
Marc Foley <m.foley.88@gmail.com>
2018-09-29 17:25:23 +0100

    Added creds for github api

--- run_fb_on_updated_fonts.sh
+++ run_fb_on_updated_fonts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo `curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files"`
-file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
+echo `curl -v -H "Authorization: token $GH_TOKEN" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files"`
+file_names=`curl -v -H "Authorization: token $GH_TOKEN" "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
 echo $file_names
 fontbakery check-googlefonts $file_names -l FAIL -n
