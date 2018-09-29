27bab9a404c1dce83301edee9cebb72dc8eae6a4
Marc Foley <m.foley.88@gmail.com>
2018-09-29 17:08:04 +0100

    travis: disable progress bar

--- run_fb_on_updated_fonts.sh
+++ run_fb_on_updated_fonts.sh
@@ -2,4 +2,4 @@
 echo `curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files"`
 file_names=`curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files" | jq '.[] | .filename' | tr '\n' ' ' | tr '"' ' '`
 echo $file_names
-fontbakery check-googlefonts $file_names -l FAIL
+fontbakery check-googlefonts $file_names -l FAIL -n
