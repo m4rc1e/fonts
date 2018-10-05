109269c12eb54d5c7e906d4e08ccf7741157ef23
Marc Foley <m.foley.88@gmail.com>
2018-10-04 21:55:45 -0400

    Test msg post from python

--- viz_pr.py
+++ viz_pr.py
@@ -1,29 +1,37 @@
 import sys
 import os
 import requests
+import json
 # from diffbrowsers.diffbrowsers import DiffBrowsers
 # from diffbrowsers.browsers import gdi_browsers, all_browsers
 
 IMG_DIR = 'imgs'
 
 
-def post_images_to_imgur(paths):
-    """Post images to hosting service imgur."""
-    upload_urls = []
-    for path in paths:
-        r = requests.post('https://api.imgur.com/3/image',
-            data={'image': open(path, 'rb').read(), 'type': 'file'},
-            headers = {'Authorization': 'Client-ID {}'.format(os.environ['IMGUR_CLIENT_ID'])}
-        )
-        upload_urls.append(r.json()['data']['link'])
-        time.sleep(1)
-    return upload_urls
+# def post_images_to_imgur(paths):
+#     """Post images to hosting service imgur."""
+#     upload_urls = []
+#     for path in paths:
+#         r = requests.post('https://api.imgur.com/3/image',
+#             data={'image': open(path, 'rb').read(), 'type': 'file'},
+#             headers = {'Authorization': 'Client-ID {}'.format(os.environ['IMGUR_CLIENT_ID'])}
+#         )
+#         upload_urls.append(r.json()['data']['link'])
+#         time.sleep(1)
+#     return upload_urls
+
+
+def post_gh_msg():
+    r = requests.post("https://api.github.com/repos/{}/issues/{}/comments".format(os.environ['TRAVIS_REPO_SLUG'], os.environ['TRAVIS_PULL_REQUEST']),
+        data=json.dumps({'body': 'Hello World!'}),
+        headers={'Authorization': 'token {}'.format(os.environ['GH_TOKEN'])})
+    print(r.text)
+
 
 
 def ci_diff():
-    auth = (os.environ['BSTACK_USERNAME'], os.environ['BSTACK_ACCESS_KEY'])
-    print(auth)
-    print(os.environ['IMGUR_CLIENT_ID'])
+    post_gh_msg()
+    # auth = (os.environ['BSTACK_USERNAME'], os.environ['BSTACK_ACCESS_KEY'])
     # diff = DiffBrowsers(auth, 'google-fonts', fonts_after, dst_dir=IMG_DIR)
     # diff.diff_View('waterfall', gen_gifs=True)
     # diff.update_browsers(gdi_browsers)
