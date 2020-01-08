392aebaf401d5e07cf57c96cbca7b3dbc3351c62
Marc Foley <m.foley.88@gmail.com>
2020-01-08 13:40:05 +0000

    fix run.sh

--- .ci/run.sh
+++ .ci/run.sh
@@ -3,7 +3,7 @@
 PR_FILES=https://github.com/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files
 HAS_FONTS=$(curl -H "Authorization: token $GH_TOKEN" $PR_FILES | jq '[.[] | .filename | test("ttf|otf")] | any')
 
-if [ $HAS_FONTS == true ];
+if [ $HAS_FONTS = true ];
 then
     echo "Running gftools qa"
     gftools qa -pr https://github.com/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST -gfb -a -o qa -ogh
