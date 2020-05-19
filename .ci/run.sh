7d2d0eee1bb137a29eba48e5d5f6e23ab6a0a02c
Marc Foley <m.foley.88@gmail.com>
2020-05-19 19:32:10 +0100

    Migrate CI to Github Actions
    Replaces current Travis setup.
    
    Benefits include:
    - Faster
    - Multiple families can now be included in a single PR
    - PRs can come from forked repos
    
    Fixes #2463

--- .ci/run.sh
+++ .ci/run.sh
@@ -1,12 +1,21 @@
-# Only run gftools qa on prs which contain font files.
+# Only run gftools qa on prs which have modified files in directories which
+# contain font binaries.
 
-PR_FILES=https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files
-HAS_FONTS=$(curl -H "Authorization: token $GH_TOKEN" $PR_FILES | jq '[.[] | .filename | test("ttf|otf")] | any')
+# Find directories which contain files that have been altered or added. Also
+# Skip /static directories.
+CHANGED_DIRS=$(git diff origin/master HEAD --dirstat | sed "s/[0-9. ].*%//g" | grep -v "static")
+OUT=out
 
-if [ "$HAS_FONTS" = true ];
-then
-    echo "Running gftools qa"
-    gftools qa -pr https://github.com/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST -gfb -a -o qa -ogh
-else
-    echo "Skipping. No fonts in PR"
-fi
+for dir in $CHANGED_DIRS
+do
+    font_count=$(ls -1 $dir*.ttf 2>/dev/null | wc -l)
+    if [ $font_count != 0 ]
+    then
+	echo "Checking $dir"
+	mkdir -p $OUT
+        gftools qa -f $dir*.ttf -gfb -a -o $OUT/$(basename $dir)_qa
+    else
+	echo "Skipping $dir. Directory does not contain fonts"
+    fi
+done
+
