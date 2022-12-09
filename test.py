d9d9ea138b2157ac87528a862c25ee5da8218aac
Marc Foley <m.foley.88@gmail.com>
2022-12-09 17:39:34 +0000

    update test

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-#!/usr/bin/env python3
-"""
-"""
-import os
-from fontTools.ttLib import TTFont
-from diffenator import ninja_proof, ninja_diff
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+#chrome_options.add_argument("--disable-extensions")
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--headless")
 
-ttFonts = [TTFont("ofl/mavenpro/MavenPro[wght].ttf")]
-ninja_proof(ttFonts, out="out", imgs=True)
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
